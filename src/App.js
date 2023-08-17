f08b302f257cc6ed538052ce24decfe05f2c616f
Your Name <you@example.com>
2023-08-17 16:23:06 +0300

    app-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
+import AppRouter from "./router/AppRouter";
 
 const App = () => {
   return (
     <div>
-      <Register />
+      <AppRouter/>
     </div>
   );
 };
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./../pages/Main";
+import Login from "./../pages/Login";
+import Navbar from "../components/Navbar";
+import Register from './../pages/Register';
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
