e92f55c678b5bf5b0b85540a9d3bf50afa0361bd
Your Name <you@example.com>
2023-08-18 17:11:47 +0300

    moviecard complated

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MovieContext } from "../context/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 const Main = () => {
-  return <div>Main</div>;
+  const { movies } = useContext(MovieContext);
+  return (
+    <div>
+      <div className="flex justify-center flex-wrap">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} {...movie} />
+        ))}
+      </div>
+    </div>
+  );
 };
-
 export default Main;
